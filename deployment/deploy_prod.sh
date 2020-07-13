d7001be9fc370ef503e2d552c399c42269350bef
CHIRANJEEVI PANCHAKARLA <chirumighty@yahoo.com>
2020-07-13 15:48:28 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,7 +4,6 @@ ssh AzureUser@52.177.124.28 <<EOF
   cd djtrump
   git pull
   source ~/env/bin/activate
-  pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
